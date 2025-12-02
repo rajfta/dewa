d6fbb96305b83bf9d1f787d76f6387a01b5f7ec8
rajfta <rajfta@gmail.com>
2025-12-02 08:19:19 +0100

    fix bezar

--- components/Product.tsx
+++ components/Product.tsx
@@ -14,6 +14,7 @@ import ReactMarkdown from "react-markdown";
 import type { ProductType } from "../types";
 
 import { Button } from "./uikit";
+import { useTranslations } from "next-intl";
 
 type ProductProps = {
     product: ProductType;
@@ -61,6 +62,8 @@ const ProductModal = ({
     product,
     onClose,
 }: ProductModalProps) => {
+    const t = useTranslations("products");
+
     const handleClose = () => {
         setOpen(false);
         if (onClose) {
@@ -126,7 +129,7 @@ const ProductModal = ({
                                             className="inline-flex justify-center flex-1 rounded-md border-transparent shadow-sm px-4 py-2 text-base font-medium focus:outline-none focus:ring-transparent text-white sm:text-sm"
                                             onClick={handleClose}
                                         >
-                                            Bezár
+                                            {t("close")}
                                         </Button>
                                     </div>
                                 </div>
